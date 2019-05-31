1561244b73de0a5b9101e21ec82e74d425539994
raluca-san <ralucam.sandu@gmail.com>
2019-05-31 14:20:36 +0200

    #4 add dict with source ct series and their seriesinstanceuid

--- fix_segmentations_dcm.py
+++ fix_segmentations_dcm.py
@@ -66,25 +66,44 @@ if __name__ == '__main__':
     patient_id = "MAV-M06"
     patient_dob = '19600101'
     #%% XML encoding
-    for subdir, dirs, files in os.walk(rootdir):
-
-        for file in sorted(files):  # sort files by date of creation
-            fileName, fileExtension = os.path.splitext(file)
-
-            if fileExtension.lower().endswith('.xml'):
-                xmlFilePathName = os.path.join(subdir, file)
-                xmlfilename = os.path.normpath(xmlFilePathName)
-                encode_xml(xmlfilename, patient_id, patient_name, patient_dob)
+    # for subdir, dirs, files in os.walk(rootdir):
+    #     for file in sorted(files):  # sort files by date of creation
+    #         fileName, fileExtension = os.path.splitext(file)
+    #         if fileExtension.lower().endswith('.xml'):
+    #             xmlFilePathName = os.path.join(subdir, file)
+    #             xmlfilename = os.path.normpath(xmlFilePathName)
+    #             encode_xml(xmlfilename, patient_id, patient_name, patient_dob)
 
     #%% DICOM encoding
+    # todo: assign correctly the series instance from plan series to tumour segm
+    # todo: assign correctly the series instance from validation series to ablation segm
     for subdir, dirs, files in os.walk(rootdir):
+        # study_0, study_1 case?
+        if 'Series_' in subdir:
+            # get the source image sequence attribute - SOPClassUID
+            for file in sorted(files):
+
+                DcmFilePathName = os.path.join(subdir, file)
+                dcm_file_source = os.path.normpath(DcmFilePathName)
+                try:
+                    dataset_source_ct = pydicom.read_file(dcm_file)
+                    source_series_instance_uid = dataset_source_ct.SeriesInstanceUID
+                    source_series_number = dataset_source_ct.SeriesNumber
+                    list_all_ct_series = []
+                    dict_series_folder = {"SeriesNumber":  source_series_number,
+                                          "SeriesInstanceNumber": source_series_instance_uid
+                                          }
+                    dict_series_folder.append(dict_series_folder)
+                    break  # exit loop, we only need the first file
+                except Exception:
+                    # not dicom file so continue until you find one
+                    continue
+
         if 'SeriesNo_' and 'Segmentations' in subdir:
             for file in sorted(files):  # sort files by date of creation
                 DcmFilePathName = os.path.join(subdir, file)
                 try:
                     k = 1
-                    # modify only the segmentation files --> if the files are in the "Segmentations" files
-                    # InstanceNumber, SliceLocation, SOPInstanceUID
                     dcm_file = os.path.normpath(DcmFilePathName)
                     dataset = pydicom.read_file(dcm_file)
                     dataset.PatientName = patient_name
@@ -92,11 +111,18 @@ if __name__ == '__main__':
                     dataset.PatientBirthDate = patient_dob
                     dataset.InstitutionName = "None"
                     dataset.InstitutionAddress = "None"
-                    # dataset.SOPInstanceUID = make_uid()
                     dataset.SliceLocation = dataset.ImagePositionPatient[2]
                     dataset.SOPInstanceUID = uid.generate_uid()
                     dataset.InstanceNumber = k
                     k += 1
+                    dataset.ImageType = "DERIVED\SECONDARY\AXIAL"
+
+                    # find the source series instance uid based on the source series number
+
+                    dataset.SourceInstanceSequence = source_series_instance_uid
+                    dataset.DerivationDescription = source_series_number
+
+                    # get the series number
                     dataset.save_as(dcm_file)
 
                 except Exception as e:
