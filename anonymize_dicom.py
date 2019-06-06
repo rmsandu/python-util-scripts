102912d5b4c1b2f3b9853c0afb97c12f1548d673
raluca-san <ralucam.sandu@gmail.com>
2019-06-06 21:35:25 +0200

    encode patient info in folder dicom files

---
+++ anonymize_dicom.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on June 2019
+
+@author: Raluca Sandu
+"""
+import os
+import pydicom
+from pydicom import uid
+import xml.etree.ElementTree as ET
+from anonymization_xml_logs import encode_xml
+#%%
+
+if __name__ == '__main__':
+
+    rootdir = r""
+    patient_name = "MAV-STO-M07"
+    patient_id = "MAV-M07"
+    patient_dob = '19490101'
+    #%% XML encoding
+    for subdir, dirs, files in os.walk(rootdir):
+
+        for file in sorted(files):  # sort files by date of creation
+            fileName, fileExtension = os.path.splitext(file)
+
+            if fileExtension.lower().endswith('.xml'):
+                xmlFilePathName = os.path.join(subdir, file)
+                xmlfilename = os.path.normpath(xmlFilePathName)
+                encode_xml(xmlfilename, patient_id, patient_name, patient_dob)
+
+    #%% DICOM encoding
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in sorted(files):  # sort files by date of creation
+            DcmFilePathName = os.path.join(subdir, file)
+            try:
+                dcm_file = os.path.normpath(DcmFilePathName)
+                dataset = pydicom.read_file(dcm_file)
+                dataset.PatientName = patient_name
+                dataset.PatientID = patient_id
+                dataset.PatientBirthDate = patient_dob
+                dataset.InstitutionName = "None"
+                dataset.InstitutionAddress = "None"
+                dataset.save_as(dcm_file)
+
+            except Exception as e:
+                pass
+                print("Cannot parse this file as DICOM:", DcmFilePathName + " - " + repr(e))
+
+print("Patient Folder Segmentations Encoded:", patient_name)
+
+
